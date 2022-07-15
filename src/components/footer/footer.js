107e3cc854e81dbbbd4ca3212c0c39c445c000cb
maureennduta <mnduta57@gmail.com>
2022-07-15 10:58:34 +0300

    responsive content-done

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,35 +1,33 @@
 import React from "react";
 import "./footer.scss";
-import {
-  LogoFacebook,
-  LogoTwitter,
-  LogoInstagram,
-} from "@carbon/icons-react";
+import { LogoFacebook, LogoTwitter, LogoInstagram } from "@carbon/icons-react";
 
 const Footer = () => (
-  <div className="footer">
-    <div class="footer-dark">
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col item social">
-              <a href="/">
-                <LogoFacebook/>
-              </a>
-              <a href="/">
-                <LogoTwitter />
-              </a>
-              <a href="/">
-                <LogoInstagram />
-              </a>
+  <div className="column">
+    <div className="footer">
+      <div class="footer-dark">
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col item social">
+                <a href="/">
+                  <LogoFacebook />
+                </a>
+                <a href="/">
+                  <LogoTwitter />
+                </a>
+                <a href="/">
+                  <LogoInstagram />
+                </a>
+              </div>
             </div>
+            <p class="copyright">
+              {" "}
+              <h3>ⓒ Intellectual Property Offerings: Your own IPO</h3>
+            </p>
           </div>
-          <p class="copyright">
-            {" "}
-            <h3>ⓒ Intellectual Property Offerings: Your own IPO</h3>
-          </p>
-        </div>
-      </footer>
+        </footer>
+      </div>
     </div>
   </div>
 );
